f1ef900a8c8db3c31f8fd9efd1a5133177cce7ca
Matteo Tucat <matteo.tucat@student.manchester.ac.uk>
2024-05-06 22:53:40 +0100

    Added support for multiple param groups

--- dGClip.py
+++ dGClip.py
@@ -17,6 +17,7 @@ class dGClip(torch.optim.Optimizer):
         lr: float,
         gamma: float = 0.1,
         delta: float = 0.001,
+        weight_decay: float = 0,
     ) -> None:
         if lr <= 0:
             raise ValueError("Learning rate (eta) is not > 0.")
@@ -24,33 +25,36 @@ class dGClip(torch.optim.Optimizer):
             raise ValueError("Gradient norm threshold (gamma) is not > 0.")
         if delta < 0:
             raise ValueError("delta is not >= 0.")
-        defaults = dict(lr=lr, gamma=gamma, delta=delta)
+        if weight_decay < 0:
+            raise ValueError("Weight decay is not >= 0.")
+        defaults = dict(lr=lr, gamma=gamma, delta=delta, weight_decay=weight_decay)
         super().__init__(params, defaults)
 
+    @torch.no_grad()
     def step(self, closure=None) -> None:
         loss = None
         if closure is not None:
             with torch.enable_grad():
                 loss = closure()
 
-        assert (
-            len(self.param_groups) == 1
-        ), f"{self.__class__.__name__} only supports 1 parameter group."
-        param_group = self.param_groups[0]
-        parameter_list = param_group["params"]
-        concatenated_weights = torch.cat(
-            [param.grad.view(-1) for param in parameter_list]
-        )
-        gradient_norm = torch.norm(concatenated_weights)
-
-        if gradient_norm != 0:
-            step_size = param_group["lr"] * min(
-                1, max(param_group["delta"], param_group["gamma"] / gradient_norm)
+        for param_group in self.param_groups:
+            param_group = self.param_groups[0]
+            parameter_list = param_group["params"]
+            concatenated_weights = torch.cat(
+                [param.grad.view(-1) for param in parameter_list]
             )
-        else:
-            step_size = 0
+            gradient_norm = torch.norm(concatenated_weights)
 
-        for p in param_group["params"]:
-            p.data.add_(p.grad.data, alpha=-step_size)
+            if gradient_norm != 0:
+                step_size = param_group["lr"] * min(
+                    1, max(param_group["delta"], param_group["gamma"] / gradient_norm)
+                )
+            else:
+                step_size = 0
+
+            for p in param_group["params"]:
+                if param_group["weight_decay"]:
+                    p.grad = p.grad.add(p, alpha=param_group["weight_decay"])
+                p.add_(p.grad, alpha=-step_size)
 
         return loss
